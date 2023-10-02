e14e0a2a241229aebf0bca264994b11518253137
Hugo <hugoo.ribeiro03@gmail.com>
2023-10-02 17:52:11 -0300

    versao novo dump

--- dump.sql
+++ dump.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 12.16 (Ubuntu 12.16-0ubuntu0.20.04.1)
 -- Dumped by pg_dump version 15.4
 
--- Started on 2023-10-02 11:53:15
+-- Started on 2023-10-02 17:51:29
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -31,7 +31,7 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- TOC entry 209 (class 1259 OID 57408)
+-- TOC entry 202 (class 1259 OID 65536)
 -- Name: counts; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -39,36 +39,13 @@ CREATE TABLE public.counts (
     id integer NOT NULL,
     "urlID" integer,
     "userID" integer,
-    "visitCount" integer,
+    "visitCount" integer DEFAULT 0 NOT NULL,
     "createdAt" timestamp without time zone
 );
 
 
 --
--- TOC entry 208 (class 1259 OID 57406)
--- Name: counts_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
-
-CREATE SEQUENCE public.counts_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
---
--- TOC entry 2969 (class 0 OID 0)
--- Dependencies: 208
--- Name: counts_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
-ALTER SEQUENCE public.counts_id_seq OWNED BY public.counts.id;
-
-
---
--- TOC entry 207 (class 1259 OID 57377)
+-- TOC entry 203 (class 1259 OID 65540)
 -- Name: sessions; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -81,30 +58,7 @@ CREATE TABLE public.sessions (
 
 
 --
--- TOC entry 206 (class 1259 OID 57375)
--- Name: sessions_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
-
-CREATE SEQUENCE public.sessions_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
---
--- TOC entry 2970 (class 0 OID 0)
--- Dependencies: 206
--- Name: sessions_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
-ALTER SEQUENCE public.sessions_id_seq OWNED BY public.sessions.id;
-
-
---
--- TOC entry 205 (class 1259 OID 57361)
+-- TOC entry 204 (class 1259 OID 65546)
 -- Name: urls; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -118,30 +72,7 @@ CREATE TABLE public.urls (
 
 
 --
--- TOC entry 204 (class 1259 OID 57359)
--- Name: urls_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
-
-CREATE SEQUENCE public.urls_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
---
--- TOC entry 2971 (class 0 OID 0)
--- Dependencies: 204
--- Name: urls_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
-ALTER SEQUENCE public.urls_id_seq OWNED BY public.urls.id;
-
-
---
--- TOC entry 203 (class 1259 OID 57346)
+-- TOC entry 205 (class 1259 OID 65552)
 -- Name: users; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -154,152 +85,7 @@ CREATE TABLE public.users (
 );
 
 
---
--- TOC entry 202 (class 1259 OID 57344)
--- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
-
-CREATE SEQUENCE public.users_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
---
--- TOC entry 2972 (class 0 OID 0)
--- Dependencies: 202
--- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
-ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
-
-
---
--- TOC entry 2820 (class 2604 OID 57411)
--- Name: counts id; Type: DEFAULT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.counts ALTER COLUMN id SET DEFAULT nextval('public.counts_id_seq'::regclass);
-
-
---
--- TOC entry 2819 (class 2604 OID 57380)
--- Name: sessions id; Type: DEFAULT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.sessions ALTER COLUMN id SET DEFAULT nextval('public.sessions_id_seq'::regclass);
-
-
---
--- TOC entry 2818 (class 2604 OID 57364)
--- Name: urls id; Type: DEFAULT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.urls ALTER COLUMN id SET DEFAULT nextval('public.urls_id_seq'::regclass);
-
-
---
--- TOC entry 2817 (class 2604 OID 57349)
--- Name: users id; Type: DEFAULT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
-
-
---
--- TOC entry 2834 (class 2606 OID 57413)
--- Name: counts counts_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.counts
-    ADD CONSTRAINT counts_pkey PRIMARY KEY (id);
-
-
---
--- TOC entry 2830 (class 2606 OID 57385)
--- Name: sessions sessions_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.sessions
-    ADD CONSTRAINT sessions_pkey PRIMARY KEY (id);
-
-
---
--- TOC entry 2832 (class 2606 OID 57387)
--- Name: sessions sessions_token_key; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.sessions
-    ADD CONSTRAINT sessions_token_key UNIQUE (token);
-
-
---
--- TOC entry 2828 (class 2606 OID 57369)
--- Name: urls urls_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.urls
-    ADD CONSTRAINT urls_pkey PRIMARY KEY (id);
-
-
---
--- TOC entry 2822 (class 2606 OID 57358)
--- Name: users users_email_key; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT users_email_key UNIQUE (email);
-
-
---
--- TOC entry 2824 (class 2606 OID 57356)
--- Name: users users_name_key; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT users_name_key UNIQUE (name);
-
-
---
--- TOC entry 2826 (class 2606 OID 57354)
--- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
-
-
---
--- TOC entry 2837 (class 2606 OID 57414)
--- Name: counts counts_urlID_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.counts
-    ADD CONSTRAINT "counts_urlID_fkey" FOREIGN KEY ("urlID") REFERENCES public.urls(id);
-
-
---
--- TOC entry 2836 (class 2606 OID 57388)
--- Name: sessions sessions_userID_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.sessions
-    ADD CONSTRAINT "sessions_userID_fkey" FOREIGN KEY ("userID") REFERENCES public.users(id);
-
-
---
--- TOC entry 2835 (class 2606 OID 57370)
--- Name: urls urls_userID_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.urls
-    ADD CONSTRAINT "urls_userID_fkey" FOREIGN KEY ("userID") REFERENCES public.users(id);
-
-
--- Completed on 2023-10-02 11:53:15
+-- Completed on 2023-10-02 17:51:29
 
 --
 -- PostgreSQL database dump complete
